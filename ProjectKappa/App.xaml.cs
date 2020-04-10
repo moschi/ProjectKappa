37b9b7c6a3bf4f1adbb3faeeb3402f14abd97fce
unknown <moritzschiesser@gmail.com>
2020-04-10 15:08:25 +0200

    fix exception handling

--- ProjectKappa/App.xaml.cs
+++ ProjectKappa/App.xaml.cs
@@ -15,6 +15,7 @@ namespace ProjectKappa
     {
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
+            e.Handled = true;
             if(MessageBox.Show(e.Exception.Message + $"{Environment.NewLine}Do you want to copy the StackTrace to your clipboard?.", "An Exception occured", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
             {
                 Clipboard.SetText(e.Exception.StackTrace);
