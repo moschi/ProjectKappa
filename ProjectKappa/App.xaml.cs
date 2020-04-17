60ab498e6f7e9f7652a375cf3e7f8bce38c47be5
unknown <moritzschiesser@gmail.com>
2020-04-17 17:33:19 +0200

    minor changes

--- ProjectKappa/App.xaml.cs
+++ ProjectKappa/App.xaml.cs
@@ -16,7 +16,7 @@ namespace ProjectKappa
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
-            if(MessageBox.Show(e.Exception.Message + $"{Environment.NewLine}Do you want to copy the StackTrace to your clipboard?.", "An Exception occured", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
+            if(MessageBox.Show(e.Exception.Message + $"{Environment.NewLine}Do you want to copy the StackTrace to your clipboard?", "An Exception occured", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
             {
                 Clipboard.SetText(e.Exception.StackTrace);
             }
